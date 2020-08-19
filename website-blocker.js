c1400f968a813f3a473703b4fd13a78758c6661b
Григорий Каданер <39625405+MrGregor228@users.noreply.github.com>
2020-08-19 13:14:25 +0300

    Update website-blocker.js

--- website-blocker.js
+++ website-blocker.js
@@ -1,4 +1,17 @@
-export const blockWebsiteInit = () => {
-    // let date = new Date;
-    document.write('Где оплата?');
-}; 
+export let blockWebsiteInit = () => {
+    let date = new Date(),
+        options = {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric"
+        },
+        timeOptions = {
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"
+        },
+        fullDate = date.toLocaleString("ru", options),
+        fullTime = date.toLocaleString("ru", timeOptions);
+
+    document.body.textContent = `Где оплата? ${fullDate} ${fullTime}`;
+};
